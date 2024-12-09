025fcb4715e7e11e52554c650ea3e9b73ba07ec2
imen <benmohamedimenseif@gmail.com>
2024-12-09 15:46:58 +0100

    add react redux packages

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-redux', 
-      'redux-persist',
-       'redux-persist/integration/react',
-        '@reduxjs/toolkit',
-        'redux-persist/lib/storage'
-      ], // Add react-redux to external if necessary
-    },
-    
-    
-  },
 })
